8c81c118a2a4971e93e68590ca181fecacd56c86
IrakLobster <skillet2828@mail.ru>
2022-05-24 18:41:41 +0400

    Commit

---
+++ src/main/java/SweetGiftProject.java
@@ -0,0 +1,29 @@
+public class SweetGiftProject {
+
+    public static void main(String[] args) {
+        Candy candy1 = new Candy("Snickers", 200F, 100.45F, "black");
+
+        Candy candy2 = new Candy();
+        candy2.setName("Pop");
+        candy2.setWeight(350);
+        candy2.setPrice(249.99F);
+        candy2.setColor("orange");
+
+        Marshmallow marshmallow1 = new Marshmallow("Haribo",300F, 124.99F, "cylinder");
+
+        Cake cake1 = new Cake("Gingerbread", 400F, 199.99F, "ginger");
+
+        Gift box[] = {candy1, candy2, marshmallow1, cake1};
+
+        float totalWeight = 0, totalPrice = 0;
+        for (Gift someGift : box) {
+            totalWeight += someGift.getWeight();
+            totalPrice += someGift.getPrice();
+
+            System.out.println(someGift.toString());
+        }
+
+        System.out.println("\nОбщий вес подарка: " + totalWeight + " грамм \nОбщая цена подарка: " + totalPrice + " руб.");
+
+    }
+}
